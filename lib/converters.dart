15bab0cebe8d096ac9c1f374eb77793875506e7f
dfourcfive <i.benadidou@esi-sba.dz>
2022-11-16 12:10:50 +0100

    add converters and validators

---
+++ lib/converters.dart
@@ -0,0 +1,21 @@
+import 'unicodes.dart';
+import 'validators.dart';
+
+dynamic toUnicode(arrangement, {int color = 0}) {
+  if (!isValidArrangement(arrangement)) return false;
+  //const type =  arrangement.runtimeType;
+  List<String> converted = toUpperCase(arrangement).map((piece) => UNICODE[piece]![color == 1 ? 1 : 0]).toList();
+  return converted;
+}
+
+List<String> toUpperCase(dynamic arrangement) {
+  //if (!isValidArrangement(arrangement)) return false;
+  //String type =  arrangement.runtimeType.toString();
+  List<String> converted = [];
+  if (arrangement is String) {
+    converted = arrangement.runes.map((e) => String.fromCharCode(e).toString().toUpperCase()).toList();
+  } else {
+    converted = converted.map((e) => e.toUpperCase()).toList();
+  }
+  return converted;
+}
--- lib/validators.dart
+++ lib/validators.dart
@@ -3,3 +3,40 @@ bool isValidID(id) {
 }
 
 bool isInteger(num value) => (value % 1) == 0;
+
+int countPiece(String piece, String arrngs) {
+  return (RegExp(piece).allMatches(arrngs).toList().length);
+}
+
+bool isValidArrangement(dynamic arrangement) {
+  String arrngs = "";
+  if (arrangement is String) {
+    arrngs = arrangement;
+  }
+  if (arrangement.length != 8) return false;
+
+  if (arrangement is List<String>) {
+    arrngs = arrangement.join('').toUpperCase();
+  }
+
+  // Check the presence of all pieces
+  if (countPiece('K', arrngs) != 1 ||
+      countPiece('Q', arrngs) != 1 ||
+      countPiece('R', arrngs) != 2 ||
+      countPiece('B', arrngs) != 2 ||
+      countPiece('N', arrngs) != 2) {
+    return false;
+  }
+
+  // Check the positions of bishops (on different colored squares)
+  int b1 = arrngs.indexOf('B');
+  int b2 = arrngs.lastIndexOf('B');
+  if (!((b2 - b1) % 2 != 0)) return false;
+
+  // Check the positions of rooks (one on each side of the king)
+  int k = arrngs.indexOf('K');
+  int r1 = arrngs.indexOf('R');
+  if (r1 > k) return false;
+
+  return true;
+}
