ed8c7aabd7e19fe0fd135bea2bc1c9db9d043a4f
dfourcfive <i.benadidou@esi-sba.dz>
2022-11-16 12:50:05 +0100

    add docs for converters class functions

--- lib/converters.dart
+++ lib/converters.dart
@@ -8,6 +8,13 @@ dynamic toUnicode(arrangement, {int color = 0}) {
   return converted;
 }
 
+/**
+ * Mirrors a starting position's arrangement of pieces (its "twin").
+ *
+ * param arrangement A starting position's arrangement
+ * returns The mirrored arrangement of pieces, or `false` if
+ *    invalid arrangement
+ */
 dynamic toMirror(arrangement) {
   if (!isValidArrangement(arrangement)) return false;
   List<String> converted = [];
@@ -19,12 +26,38 @@ dynamic toMirror(arrangement) {
   return converted;
 }
 
+/**
+ * Converts an arrangement of pieces to uppercase letters.
+ *
+ * the param arrangement A starting position's arrangement
+ * returns The same arrangement in uppercase letters, or
+ *    `false` if invalid arrangement
+ */
 List<String> toUpperCase(dynamic arrangement) {
   //if (!isValidArrangement(arrangement)) return false;
   //String type =  arrangement.runtimeType.toString();
   List<String> converted = [];
   if (arrangement is String) {
     converted = arrangement.runes.map((e) => String.fromCharCode(e).toString().toUpperCase()).toList();
+  } else {
+    converted = converted.map((e) => e.toUpperCase()).toList();
+  }
+  return converted;
+}
+
+/**
+ * Converts an arrangement of pieces to uppercase letters.
+ *
+ * the param arrangement A starting position's arrangement
+ * returns The same arrangement in lowercase letters, or
+ *    `false` if invalid arrangement
+ */
+List<String> toLowerCase(dynamic arrangement) {
+  //if (!isValidArrangement(arrangement)) return false;
+  //String type =  arrangement.runtimeType.toString();
+  List<String> converted = [];
+  if (arrangement is String) {
+    converted = arrangement.runes.map((e) => String.fromCharCode(e).toString().toLowerCase()).toList();
   } else {
     converted = converted.map((e) => e.toUpperCase()).toList();
   }
