d066c4017d653f18ae947253104197cfeb3b77d6
dfourcfive <i.benadidou@esi-sba.dz>
2022-11-16 12:47:09 +0100

    add to mirro

--- lib/converters.dart
+++ lib/converters.dart
@@ -8,6 +8,17 @@ dynamic toUnicode(arrangement, {int color = 0}) {
   return converted;
 }
 
+dynamic toMirror(arrangement) {
+  if (!isValidArrangement(arrangement)) return false;
+  List<String> converted = [];
+  if (arrangement is String) {
+    converted = arrangement.runes.map((e) => String.fromCharCode(e).toString()).toList().reversed.toList();
+  } else if (arrangement is List<String>) {
+    converted = arrangement.reversed.toList();
+  }
+  return converted;
+}
+
 List<String> toUpperCase(dynamic arrangement) {
   //if (!isValidArrangement(arrangement)) return false;
   //String type =  arrangement.runtimeType.toString();
